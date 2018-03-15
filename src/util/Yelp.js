9a4da4cc510702fc76ca2c991e4ad2b81cbc7956
AGit <30912338+AdamGitFun@users.noreply.github.com>
2018-03-15 17:17:00 -0400

    fixes syntax error, finally works

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -3,10 +3,10 @@ const Yelp = {
   search(term, location, sortBy) {
     return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, {
       headers: {
-        Authorization: 'Bearer' + apiKey
+        Authorization: 'Bearer ' + apiKey
       }
     }).then( response => {
-      console.log(response.json());
+      console.log(response);
       return response.json();
     }).then( jsonResponse => {
       if (jsonResponse.businesses) {
